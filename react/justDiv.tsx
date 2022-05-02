7584192dbf8bde513b56ff18b903de63ce000d09
Marco Gatto <m.gatto@reply.it>
2022-05-02 17:56:12 +0200

    Scroll event listener implemented

--- react/justDiv.tsx
+++ react/justDiv.tsx
@@ -1,32 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useCssHandles } from 'vtex.css-handles'
 
 interface justDivProps {
   blockClass?: string,
-  htmlId?: string
+  htmlId?: string,
+  minPosition?: number
 
 }
 
 const CSS_HANDLES = [
-  'justDiv',
+  'justDiv'
 ] as const;
 
 const justDiv: StorefrontFunctionComponent<justDivProps> = (
   { 
     children,
     blockClass="",
-    htmlId
+    htmlId,
+    minPosition=0
   }
   
-  ) => {
+) => {
+
+  let lastPosition = 0;
+
+  const [scrollDirection, setScrollDirection] = useState("none");
+
+  const handleScroll = () => {
+    const actualPosition = window.pageYOffset;
+    let direction = "none";
+    if(actualPosition < minPosition) {
+      direction = "none";
+    } else if(lastPosition < actualPosition) {
+      direction = "down";
+    } else if(lastPosition > actualPosition) {
+      direction = "up";
+    }
+    lastPosition = actualPosition;
+    setScrollDirection(direction);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const { handles } = useCssHandles(CSS_HANDLES, blockClass);
-  
-
-  return ( <div className={handles.justDiv} id={htmlId}>
-    {children}
-  </div> )
+  return (<div
+    className={handles.justDiv +
+                (scrollDirection === "up" ? "--scrollUp" : "") +
+                (scrollDirection === "down" ? "--scrollDown" : "")
+              }
+    style={scrollDirection === "up" || scrollDirection === "down" ? {position: "fixed"} : {width: "100%"}}
+    id={htmlId}>
+            {children}
+          </div>)
 }
 
 
